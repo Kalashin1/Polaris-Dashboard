cfd91c7ff8217ce4b0f64ba4390df6cb3e1646e6
kinanee Samson <kinaneeSamsonJohn@gmail.com>
2020-12-08 18:37:22 +0100

    changed the auth fle to point the url of api request to the nodejs hosted url

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -21,13 +21,13 @@ export class AuthService {
 
   addChannel(channel: Channel, user: User):Observable<any>{
     let obj = {user : user,  channel : channel}
-    return this.http.post('http://localhost:4000/add-channel', obj, {headers: new HttpHeaders({'Content-Type': 'application/json'})})
+    return this.http.post('https://youngstars-project.glitch.me/add-channel', obj, {headers: new HttpHeaders({'Content-Type': 'application/json'})})
 
   }
 
   createUser(user: User):Observable<any>{
     user.channels = []
-    return this.http.post('http://localhost:4000/create-user', user, {
+    return this.http.post('https://youngstars-project.glitch.me/create-user', user, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
@@ -35,7 +35,7 @@ export class AuthService {
   }
 
   loginUser(user: User):Observable<any>{
-    return this.http.post('http://localhost:4000/login', user, {
+    return this.http.post('https://youngstars-project.glitch.me/login', user, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
@@ -43,19 +43,15 @@ export class AuthService {
   }
 
   logoutUser():Observable<any>{
-    return this.http.get('http://localhost:4000/logout-user')
+    return this.http.get('https://youngstars-project.glitch.me/logout-user')
   }
 
   fetchUserWithId():Observable<User>{
-    return this.http.get<User>('http://localhost:4000/user')
-  }
-
-  isUserLoggedIn():Observable<boolean>{
-    return this.fetchUserWithId().pipe(map(user => {if(user!== undefined || null) return true}))
+    return this.http.get<User>('https://youngstars-project.glitch.me/user')
   }
 
   fetchChannels():Observable<Channel[]>{
-    return this.http.get<Channel[]>('http://localhost:4000/channels')
+    return this.http.get<Channel[]>('https://youngstars-project.glitch.me/channels')
   }
 
 }
